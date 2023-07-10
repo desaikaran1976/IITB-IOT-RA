9e34ff36420db13689f63fd183d87354d83b6523
Karan Desai <desaikaran1976@gmail.com>
2023-07-10 16:27:04 +0530

    Minor Changes is volt 5V to 3.3V

--- DRV425_Sensor/DRV425_Sensor.ino
+++ DRV425_Sensor/DRV425_Sensor.ino
@@ -7,12 +7,12 @@ const int8_t SYNC_BYTE = 0xAA;
 float Bvalue = 0;
 
 void setup() {
-  Serial.begin(1000000);
+  Serial.begin(1500000);
 }
 
 void loop() {
 
-  Bvalue = ((analogRead(read_pin) - 2048) * 5)/4096;
+  Bvalue = ((analogRead(read_pin) - 2048) * 3.3)/4096;
   Serial.write(SYNC_BYTE); // Send the start/sync byte
   Serial.write((uint8_t*)&(Bvalue), sizeof(Bvalue));
 
