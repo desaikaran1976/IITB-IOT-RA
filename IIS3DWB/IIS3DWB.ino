a4860a533f9748b44be05226818e62660693b0f8
desaikaran1976 <64833094+desaikaran1976@users.noreply.github.com>
2023-07-17 12:52:10 +0530

    Wifi_Functionalities Added
    IP Address :: 188

--- IIS3DWB/IIS3DWB.ino
+++ IIS3DWB/IIS3DWB.ino
@@ -2,11 +2,33 @@
 
 #include "IIS3DWB.h"
 #include "SPI.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "soc/rtc_wdt.h"
+#include "esp_int_wdt.h"
+#include "esp_task_wdt.h"
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+#include <Arduino.h>
 
 #define SerialDebug true  // set to true to get Serial output for debugging
 #define myLed    2
 #define CSPIN    5
+///////////////WiFi-Setup//////////////
+const char* ssid = "IITB_IOT";
+const char* password =  "iitbiot1234";
 
+String control_val = "";
+
+AsyncWebServer server(80);
+///////////////IIS3DWB-GET//////////////
+TaskHandle_t IIS3DWB_GET_h = NULL;
+///////////////IP-ADDRESS SETTINGS//////////////
+IPAddress staticIP(192, 168, 0, 188);
+
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress dns(0, 0, 0, 0);
 
 //IIS3DWB definitions
 #define IIS3DWB_intPin1 34  // interrupt1 pin definitions, data ready
@@ -29,7 +51,8 @@ IIS3DWB IIS3DWB(CSPIN); // instantiate IIS3DWB class
 
 void setup() 
 {
-  Serial.begin(1500000);
+  // Serial.begin(1500000);
+  Serial.begin(115200);
 //  delay(4000);
 
   SPI.begin(18,19,23,5); // Start SPI serial peripheral(CLK,MISO,MOSI,CS)
@@ -86,15 +109,67 @@ void setup()
 
  // attachInterrupt(IIS3DWB_intPin1, myinthandler1, RISING);   // define interrupt for intPin1 output of IIS3DWB
  // attachInterrupt(IIS3DWB_intPin2, myinthandler2, FALLING);  // define interrupt for intPin2 output of IIS3DWB
+
+     xTaskCreatePinnedToCore(
+        IIS3DWB_GET,     //TASK
+        "IIS3DWB_GET",   //Task id
+        8000,               //Stack Size
+        NULL,
+        1,
+        &IIS3DWB_GET_h,   //TASK HANDLE
+        1
+    );
+    
+    // vTaskResume(MPU_GET_h);
+    if (WiFi.config(staticIP, gateway, subnet, dns, dns) == false) {
+      Serial.println("Configuration failed.");
+    }
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password); 
+  // attempt to connect to Wifi network:
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+    }    
+ Serial.println(WiFi.localIP());
+
+    server.on(
+      "/post",
+      HTTP_POST,
+      [](AsyncWebServerRequest * request){},
+      NULL,
+      [](AsyncWebServerRequest * request, uint8_t *data, size_t len, size_t index, size_t total) {
+
+        for (size_t i = 0; i < len; i++) {
+          control_val += (char)data[i];
+        }
+
+        esp_task_wdt_init(10, false);
+
+        // if(control_val == "ESP_RESTART"){
+    if(control_val == "ESP_RESTART"){ 
+          request->send(200);
+          delay(500);
+          ESP.restart();
+          // vTaskSuspend(MPU_GET_h);
+          // delay(500);
+          // calibrate_MPU(Acc_bias,Gyro_bias);
+          // delay(500);
+          // vTaskResume(MPU_GET_h);
+        }
+        
+        control_val = "";
+    });
+  
+    server.begin();
 }
 /* End of setup */
 
-void loop() {
+void IIS3DWB_GET(void *parameter){
 
 //     if(IIS3DWB_DataReady)  // Handle data ready condition
 //     {
 //       IIS3DWB_DataReady = false;
-      
+    for(;;){  
      IIS3DWBstatus = IIS3DWB.DRstatus(); // read data ready status
      if (IIS3DWBstatus & 0x01) {         // if new accel data is available, read it
 
@@ -109,6 +184,7 @@ void loop() {
         Serial.write((uint8_t*)&(ay), sizeof(ay));
         Serial.write((uint8_t*)&(az), sizeof(az));
       }
+    }
    //  }  // end of data ready interrupt handling
      
 //
@@ -160,3 +236,26 @@ void loop() {
 //{
 //  IIS3DWB_Wakeup = true;
 //}
+
+void loop(){
+    delay(1000);
+  // vTaskDelete(NULL)
+  connect_to_wifi();
+}
+
+void connect_to_wifi() {
+  unsigned long lastTimeStamp = millis();
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+//     Serial.println("Connecting to WiFi..");
+
+    //if WiFI doesn't connect within 30 secs, reconnect WiFi
+    if ( (millis() - lastTimeStamp) > 30*1000 ) {  
+//       Serial.println("Failed to connect to WiFi. Restarting in 5 seconds");
+      delay(5000);
+      // ESP.restart();
+    WiFi.reconnect();
+    }
+  }
+}
